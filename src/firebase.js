1cdbddde1ef5d4f00c1135ca5e06ccede9c4be02
Arup Chandra Dawn <arupchandradawn@gmail.com>
2020-09-29 05:32:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,6 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCNNBcX_XJ8eDmUKEvHiG3cQSsfRaeqaBQ",
-    authDomain: "slack-clone-a0cc6.firebaseapp.com",
-    databaseURL: "https://slack-clone-a0cc6.firebaseio.com",
-    projectId: "slack-clone-a0cc6",
-    storageBucket: "slack-clone-a0cc6.appspot.com",
-    messagingSenderId: "277070418766",
-    appId: "1:277070418766:web:095c63422f5668e03a441f",
-    measurementId: "G-9G6L0LXK4F"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
